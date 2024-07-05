760cf25fa8be68c3dc3b519f42b46236a8e4e364
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-07-05 17:22:45 +0300

    finsih the drawer

--- lib/view/Widgets/custom_drawe.dart
+++ lib/view/Widgets/custom_drawe.dart
@@ -2,11 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:responsive_course/models/drawer_model_item.dart';
 import 'package:responsive_course/view/Widgets/custom_drawer_body.dart';
-import 'package:responsive_course/view/Widgets/custom_drawer_item.dart';
 
 class CustomDrawer extends StatelessWidget {
-  CustomDrawer({super.key});
-  final List<DrawerModelListItem> items = [
+  const CustomDrawer({super.key});
+  static List<DrawerModelListItem> items = [
     DrawerModelListItem(title: "Dashboard", icon: Icons.home),
     DrawerModelListItem(title: "Setting", icon: Icons.settings),
     DrawerModelListItem(title: "About", icon: Icons.info),
