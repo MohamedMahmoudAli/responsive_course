4cf0a75db63941d7aecc3f0ba708fbbf87ff799c
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-07-07 02:29:46 +0300

    text widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_course/Screens/HomeScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    builder: (BuildContext context) {
+      return const MyApp();
+    },
+    enabled: true,
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +21,36 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(),
-        home: const HomeScreen());
+        home: const TextPreview());
   }
+}
+
+class TextPreview extends StatelessWidget {
+  const TextPreview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("Responsive ui"),
+          Text(
+            "the label ",
+            style: TextStyle(fontSize: MediaQuery.of(context).size.width * .05),
+          ),
+          const Text(
+            "the content of the all screen",
+            style: TextStyle(fontSize: 16),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+double getResponsiveFontSize(
+
+  {required double width }
+){
+  return width * .05;
 }
