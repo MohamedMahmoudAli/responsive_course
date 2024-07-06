0c0c6f379800d2822a583d77f2102873dcaa6457
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-07-06 14:07:32 +0300

    custom app bar

--- lib/Screens/HomeScreen.dart
+++ lib/Screens/HomeScreen.dart
@@ -18,18 +18,65 @@ class _HomeScreenState extends State<HomeScreen> {
         key: scaffoldKey,
         drawer: const CustomDrawer(),
         backgroundColor: const Color(0xffDBDBDB),
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          leading: GestureDetector(
-            onTap: () {
-              scaffoldKey.currentState!.openDrawer();
-            },
-            child: const Icon(
-              Icons.menu,
-              color: Colors.white,
-            ),
-          ),
-        ),
+        appBar: MediaQuery.of(context).size.width < 900
+            ? AppBar(
+                backgroundColor: Colors.black,
+                leading: GestureDetector(
+                  onTap: () {
+                    scaffoldKey.currentState!.openDrawer();
+                  },
+                  child: const Icon(
+                    Icons.menu,
+                    color: Colors.white,
+                  ),
+                ),
+              )
+            : null,
         body: const HomeViewBody());
   }
 }
+
+class CustomAdaptiveAppBar extends StatelessWidget
+    implements PreferredSizeWidget {
+  const CustomAdaptiveAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => const Size.fromHeight(56);
+}
+
+// class CustomAdaptiveAppBar extends StatelessWidget
+//     implements PreferredSizeWidget {
+//   const CustomAdaptiveAppBar({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return LayoutBuilder(builder: (context, Constraints) {
+//       if (Constraints.maxWidth < 900) {
+//         return AppBar(
+//           backgroundColor: Colors.black,
+//           leading: GestureDetector(
+//             onTap: () {
+//               // scaffoldKey.currentState!.openDrawer();
+//             },
+//             child: const Icon(
+//               Icons.menu,
+//               color: Colors.white,
+//             ),
+//           ),
+//         );
+//       } else {
+//         return const SizedBox();
+//       }
+//     });
+//   }
+
+//   @override
+//   // TODO: implement preferredSize
+//   Size get preferredSize => const Size.fromHeight(56);
+// }
