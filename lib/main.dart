09f8a0a90592218055eec386ee8aaaa5ff949eda
MohamedMahmoudAli <mohamed238ma@gmail.com>
2024-07-07 03:11:26 +0300

    the text

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'dart:js';
+
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_course/Screens/HomeScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(DevicePreview(
+    builder: (BuildContext context) {
+      return const MyApp();
+    },
+    enabled: true,
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -15,6 +23,50 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Flutter Demo',
         theme: ThemeData(),
-        home: const HomeScreen());
+        home: const TextPreview());
   }
 }
+
+class TextPreview extends StatelessWidget {
+  const TextPreview({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text("Responsive ui"),
+          Text(
+            "the label ",
+            style: TextStyle(fontSize: MediaQuery.of(context).size.width * .05),
+          ),
+          const Text(
+            "the content of the all screen",
+            style: TextStyle(fontSize: 16),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+double getResponsiveFontSize(
+  BuildContext context, {
+  required double fontSize,
+}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+  return responsiveFontSize;
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  if (width < 600) {
+    return width / 400;
+  } else if (width < 900) {
+    return width / 700;
+  } else {
+    return width / 1000;
+  }
+}
+
